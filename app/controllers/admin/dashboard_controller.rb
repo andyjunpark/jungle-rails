5e8918376eb0745b4eee7c1e12e8115371d40e5b
Andy Junhyuk Park <pos03142ap@gmail.com>
2022-02-05 03:30:24 +0000

    add counting methods in admin/dashboard_controller and render to admin in views

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
   def show
-    # render plain: "Welcome back Junglebook!"
+    @admin_username = ENV['ADMIN_USERNAME']
+    @count_products = Product.count
+    @count_categories = Category.count
   end
 end
