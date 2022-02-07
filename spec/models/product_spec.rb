2afc6ff3a39c99bdb76db7e58c2413316062689e
Andy Junhyuk Park <pos03142ap@gmail.com>
2022-02-07 23:32:33 +0000

    add spec/models/product_spec.rb

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    # validation tests/examples here
+    validates: name, presence true
+    validates: price, presence true
+    validates: quantity, presence true
+    validates: category, presence true
+  end
+end
